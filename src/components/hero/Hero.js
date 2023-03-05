aa5f3254bc6e11defd42f53ea077f1c5dfe59036
Maryna-bond <bondarenko.mary1991@gmail.com>
2023-03-05 18:05:42 +0100

    Added some css to Hero and Header

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -5,17 +5,20 @@ class Hero extends Component {
   render() {
     return (
       <section className="hero">
-        <div className="hero-section">
-          <p className="hero-section__description-add">MODERN INTERIOR</p>
-          <h1 className="hero-section__title">Create Your Interior Design.</h1>
-          <p className="hero-section__description">
+        <div className="hero__description">
+          <p className="hero__description__subtitle">MODERN INTERIOR</p>
+          <h1 className="hero__description__title">Create Your Interior Design.</h1>
+          <p className="hero__description__story">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
             veniam, quis nostrud exercitation.
           </p>
-          <button className="hero-section__button">CONTACT</button>
+          <button className="hero__description__button">CONTACT</button>
         </div>
-        <button className='swiper-arrow-next'><img className='swiper-arrow'  src="././././img/swiper-arrow-right.png"></img></button>
+        <div className='hero__image'>
+          <img className='hero__image-design' src='./img/hero-pic.jpg' alt="design"/>
+        </div>
+        
       </section>
     );
   }
